5c000ac1abd06d981ccf50de5d040d62ef8c16f7
ZoranUTF8 <janjiczoran23@gmail.com>
2022-09-26 14:17:55 +0200

    finish about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,63 @@
 import React from "react";
 import "./About.css";
+import ME from "../../assets/me-about.jpeg";
+import { BiTimeFive } from "react-icons/bi";
+import { HiOutlineUsers } from "react-icons/hi";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 
 const About = () => {
-  return <section id="about">ABOUT </section>;
+  return (
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+        <div className="about__container__left about__me">
+          <div className="about__me-img">
+            <img src={ME} alt="About image" />
+          </div>
+        </div>
+
+        <div className="about__container__right">
+          <div className="about__content">
+            <div className="about__cards">
+              <article className="about__card">
+                <BiTimeFive className="about__icon" size={28} />
+                <h5>Experience</h5>
+                <small>6+ months working</small>
+              </article>
+              <article className="about__card">
+                <HiOutlineUsers className="about__icon" size={28} />
+                <h5>Clients</h5>
+                <small>4+ clients worldwide</small>
+              </article>
+              <article className="about__card">
+                <AiOutlineFundProjectionScreen
+                  className="about__icon"
+                  size={28}
+                />
+                <h5>Projects</h5>
+                <small>10+ completed</small>
+              </article>
+            </div>
+            <p>
+              I am a motivated Junior software developer with the ability to
+              learn and collaborate in a rapidly changing environment and
+              compositions. Currently, I am pursuing my computer science
+              bachelor's degree online at the{" "}
+              <a href="https://www.uopeople.edu/">university of the people</a>.
+              I worked through 500+ hours of Bootcamp structure, learning the
+              MERN stack and Java. Feel free to message me.
+            </p>
+          </div>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,72 @@
-import React from "react";
+import { useState } from "react";
 import "./Navbar.css";
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 import { BiBookBookmark } from "react-icons/bi";
 import { MdOutlineDesignServices } from "react-icons/md";
 import { BsInfoCircle } from "react-icons/bs";
+
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState("home");
+
+  // Change active nav
+  const handleActiveNav = (item) => {
+    switch (item) {
+      case "home":
+        setActiveNav("home");
+        break;
+      case "about":
+        setActiveNav("about");
+        break;
+      case "experience":
+        setActiveNav("experience");
+        break;
+      case "services":
+        setActiveNav("services");
+        break;
+      case "contact":
+        setActiveNav("contact");
+        break;
+      default:
+        console.log("No such option")
+        break;
+    }
+  };
+
   return (
     <nav>
-      <a href="#">
+      <a
+        href="#"
+        onClick={() => handleActiveNav("home")}
+        className={activeNav === "home" ? "active" : ""}
+      >
         <AiOutlineHome size={28} />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => handleActiveNav("about")}
+        className={activeNav === "about" ? "active" : ""}
+      >
         <AiOutlineUser size={28} />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => handleActiveNav("experience")}
+        className={activeNav === "experience" ? "active" : ""}
+      >
         <BiBookBookmark size={28} />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => handleActiveNav("services")}
+        className={activeNav === "services" ? "active" : ""}
+      >
         <MdOutlineDesignServices size={28} />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => handleActiveNav("contact")}
+        className={activeNav === "contact" ? "active" : ""}
+      >
         <BsInfoCircle size={28} />
       </a>
     </nav>
